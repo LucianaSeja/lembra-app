8c06186f66833a65fb03f0596ab79f2e69813fd5
LucianaSeja <luciana.lopes@sejadigital.com.br>
2026-03-18 22:50:38 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// ================================================================
+// COLE SUAS CREDENCIAIS DO FIREBASE AQUI
+// Copie o bloco firebaseConfig do console do Firebase e substitua
+// ================================================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkWEUBEs5krDcWC0j_EjeSXcQL9PoU1co",
+  authDomain: "lembra-app-ed9d2.firebaseapp.com",
+  projectId: "lembra-app-ed9d2",
+  storageBucket: "lembra-app-ed9d2.firebasestorage.app",
+  messagingSenderId: "194420631782",
+  appId: "1:194420631782:web:37e6a14f152add1fc97327"
+};
