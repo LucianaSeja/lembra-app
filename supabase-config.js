2bed5216f72db859c8aca7448713ca1a5fe2ed9f
LucianaSeja <luciana.lopes@sejadigital.com.br>
2026-03-23 16:00:35 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -5,4 +5,4 @@
 // ================================================================
 
 const SUPABASE_URL ='https://oziqfjawhqgyxsuzkwxy.supabase.co';
-const SUPABASE_ANON_KEY ='oziqfjawhqgyxsuzkwxy';
+const SUPABASE_ANON_KEY ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96aXFmamF3aHFneXhzdXprd3h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NDM2NTIsImV4cCI6MjA4OTUxOTY1Mn0.vwO13Gzvztz4XdvELGPriGy_U497kMXM5GKjkcuZaAc';
