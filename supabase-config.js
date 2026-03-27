383ffdcffae4920da7e7111e3454954e9d4185f6
LucianaSeja <luciana.lopes@sejadigital.com.br>
2026-03-27 20:07:22 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -4,5 +4,5 @@
 // Project URL e anon/public key
 // ================================================================
 
-const SUPABASE_URL ='https://oziqfjawhqgyxsuzkwxy.supabase.co';
-const SUPABASE_ANON_KEY ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96aXFmamF3aHFneXhzdXprd3h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NDM2NTIsImV4cCI6MjA4OTUxOTY1Mn0.vwO13Gzvztz4XdvELGPriGy_U497kMXM5GKjkcuZaAc';
+var SUPABASE_URL ='https://oziqfjawhqgyxsuzkwxy.supabase.co';
+var SUPABASE_ANON_KEY ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96aXFmamF3aHFneXhzdXprd3h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NDM2NTIsImV4cCI6MjA4OTUxOTY1Mn0.vwO13Gzvztz4XdvELGPriGy_U497kMXM5GKjkcuZaAc';
